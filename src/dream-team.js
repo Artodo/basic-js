386826eef55ec2cb9de0661ec63994da064906e6
Artur Dyshynski <tavebran@gmail.com>
2023-04-17 22:18:50 +0300

    feat: add dreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Create name of dream team based on the names of its members
  *  
@@ -13,11 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+    if (!Array.isArray(members)) {
+        return false;
+    }
+    let letters = [];
+    members.forEach(name => {
+        if (typeof name === 'string') {
+            const trimmedName = name.trim();
+            const trimmedNameArr = trimmedName.split(' ');
+            letters.push(trimmedNameArr[0][0].toUpperCase());
+        }
+    });
+    const result = letters.sort().join('');
+    return result === '' ? false : result;
 }
 
 module.exports = {
-  createDreamTeam
+    createDreamTeam
 };
